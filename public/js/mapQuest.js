6b6b0d86a9dcef6939261bce2156de5ce1c1c0a3
Danny Fung <dannyfung97@gmail.com>
2019-02-28 10:11:11 -0800

    first init

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,10 @@
 function initMap() {
-	// add your code here
+	L.mapquest.key = 'wp5SLA2UXsEd329zmaVmOapkdcddU6KP';
+	var map = L.mapquest.map('map', {
+		center: [32.88, -117.236],
+		layers: L.mapquest.tileLayer('map'),
+		zoom: 12,
+		zoomControl: false
+	});
+	L.marker([32.88, -117.236]).addTo(map);
 }
